0e7bb58674202ca6604697946544cdc2d3f6c70b
james <contact@kippcase.com>
2025-06-06 16:00:16 -0300

    color

--- init.lua
+++ init.lua
@@ -961,6 +961,9 @@ require("lazy").setup({
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			vim.cmd.colorscheme("habamax")
+vim.o.termguicolors = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg="#04110d" })
 		end,
 	},
 
